5ad2a9f5b4d0d97ddb108731cef6d9c0cd0fe7fb
irgi <irgiaditpratama@gmail.com>
2025-11-03 17:59:58 +0700

    library setup

--- mobile-solvion/app/_layout.tsx
+++ mobile-solvion/app/_layout.tsx
@@ -1,62 +1,13 @@
-import { Stack, useRouter } from "expo-router";
-import { useEffect, useState } from "react";
-import { supabase } from "../lib/supabase";
-import { Session } from "@supabase/supabase-js";
+import "../global.css"
 import { Text, View } from "react-native";
-import * as WebBrowser from "expo-web-browser";
 
-WebBrowser.maybeCompleteAuthSession();
-
-export default function RootLayout() {
-  const [session, setSession] = useState<Session | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const router = useRouter();
-
-  useEffect(() => {
-    supabase.auth
-      .getSession()
-      .then(({ data: { session } }) => {
-        setSession(session);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-
-    const { data: authListener } = supabase.auth.onAuthStateChange(
-      (_event, session) => {
-        setSession(session);
-      }
-    );
-
-    return () => {
-      authListener?.subscription.unsubscribe();
-    };
-  }, []);
-
-  useEffect(() => {
-    if (isLoading) {
-      return;
-    }
-
-    if (session) {
-      router.replace("/home");
-    } else {
-      router.replace("/");
-    }
-  }, [session, isLoading, router]);
-
-  if (isLoading) {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Loading...</Text>
-      </View>
-    );
-  }
-
+ 
+export default function App() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="home" options={{ title: "Home" }} />
-    </Stack>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt minima sequi blanditiis nesciunt, exercitationem velit quae in fuga dicta distinctio a quos optio id excepturi, nulla reprehenderit possimus aut quia?
+      </Text>
+    </View>
   );
 }
